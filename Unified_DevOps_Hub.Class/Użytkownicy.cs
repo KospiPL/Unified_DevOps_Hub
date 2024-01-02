48c32a341c28efed308c38a7527decf45b101328
KospiPL <kacper.cudzik11@gmail.com>
2024-01-02 09:38:59 +0100

    Dodanie Klienta, Api, i pierwszych Class

---
+++ Unified_DevOps_Hub.Class/Użytkownicy.cs
@@ -0,0 +1,16 @@
+﻿namespace Unified_DevOps_Hub.Class
+{
+    public class Użytkownicy
+    {
+        public int Id_Użytkownika {  get; set; }
+        public string Imie {  get; set; }
+        public string Nazwisko { get; set; }
+        public string Login { get; set; }
+        public string Haslo { get; set; }
+        public DateTime Data_Urodzenia { get; set; }
+        public string? Miejscowosc { get; set; }
+        public string? Dział { get; set; }
+        public string Email { get; set; }
+
+    }
+}
